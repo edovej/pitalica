1405c4677bca2fb5f149989e573f66f2977075d3
Edin Vejzagic <edoje2008@hotmail.com>
2020-04-22 11:55:55 +0200

    Initial Commit

---
+++ Pitalica2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pitalica2
+//
+//  Created by Edin Vejzagic on 22/4/20.
+//  Copyright © 2020 Edin Vejzagic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
